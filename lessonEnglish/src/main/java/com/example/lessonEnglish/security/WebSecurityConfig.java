d588d3fc8653c0a3bd55b9c6dbd53be883f244e9
Truong Giang <caotruonggiang1979@gmail.com>
2022-08-22 09:30:28 +0700

    code

--- lessonEnglish/src/main/java/com/example/lessonEnglish/security/WebSecurityConfig.java
+++ lessonEnglish/src/main/java/com/example/lessonEnglish/security/WebSecurityConfig.java
@@ -7,10 +7,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Bean
-//	public PasswordEncoder encoder() {
-//		return new BCryptPasswordEncoder(10);
-//	}
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder(10);
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests().antMatchers("/**").permitAll().anyRequest().authenticated();
