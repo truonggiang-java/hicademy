fbe04c402c546e8181ee411164b2ae7bb1feb338
Truong Giang <caotruonggiang1979@gmail.com>
2022-08-23 15:51:28 +0700

    code

--- lessonEnglish/src/main/java/com/example/lessonEnglish/dto/response/ResponseDto.java
+++ lessonEnglish/src/main/java/com/example/lessonEnglish/dto/response/ResponseDto.java
@@ -8,6 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResponseDto {
-	private String email;
 	private String token;
 }
--- lessonEnglish/src/main/java/com/example/lessonEnglish/security/WebSecurityConfig.java
+++ lessonEnglish/src/main/java/com/example/lessonEnglish/security/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().authorizeRequests().antMatchers("/api/v1/**").permitAll().and().antMatcher("/api/v1/**")
+				.and().authorizeRequests().antMatchers("/api/v1/user/signin").permitAll().and().antMatcher("/api/v1/**")
 				.authorizeRequests().anyRequest().authenticated();
 		
 		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
