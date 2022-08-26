5728ed0e8bb7f0882b06b814690db4796d36a598
Truong Giang <caotruonggiang1979@gmail.com>
2022-08-26 09:24:56 +0700

    code

---
+++ lessonEnglish/src/main/java/com/example/lessonEnglish/dto/UserRoleDto.java
@@ -0,0 +1,15 @@
+package com.example.lessonEnglish.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRoleDto {
+	private String name;
+	private String role;
+	private String linkUrl;
+	private String id;
+}
